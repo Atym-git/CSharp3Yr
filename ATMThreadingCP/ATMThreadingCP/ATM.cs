c728e3cd57145f141a1a65e1336c86eece84c46e
Atym-git <chuvachelaad@gmail.com>
2025-12-03 21:21:54 +0300

    init; task done

---
+++ ATMThreadingCP/ATMThreadingCP/ATM.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ATMThreadingCP
+{
+    internal class ATM
+    {
+        public static object locker = new object();
+
+        Random rnd = new Random();
+
+        public static int CurrBalance { get; private set; } = 1000;
+
+        private int _minWithdrawAmount = 5;
+        private int _maxWithdrawAmount = 20;
+
+        public ATM()
+        {
+        }
+        
+        public ATM(int minWithdrawAmount, int maxWithdrawAmount)
+        {
+            _minWithdrawAmount = minWithdrawAmount;
+            _maxWithdrawAmount = maxWithdrawAmount;
+        }
+
+        public void Withdraw(object? amountOfWithdrawTries)
+        {
+            lock (locker)
+            {
+                if (amountOfWithdrawTries is int result)
+                {
+                    for (int i = 0; i < result; i++)
+                    {
+                        int withdrawAmount = rnd.Next(_minWithdrawAmount, _maxWithdrawAmount + 1);
+
+                        WithdrawNotif();
+                        Thread.Sleep(100);
+
+                        if (CurrBalance < withdrawAmount)
+                        {
+                            Console.WriteLine("Недостаточно средств");
+                        }
+                        else
+                        {
+                            CurrBalance -= withdrawAmount;
+                        }
+
+                        Thread.Sleep(100);
+                    }
+
+                }
+                else
+                {
+                    Console.WriteLine("Данная запись не является числом");
+                }
+            }
+        }
+
+        private void WithdrawNotif() => Console.WriteLine("Пытаемся снять деньги с банкомата");
+    }
+}
---
+++ ATMThreadingCP/ATMThreadingCP/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace ATMThreadingCP
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+
+            int threadsAmount = rnd.Next(4, 7);
+            Thread[] threads = new Thread[threadsAmount];
+
+            for (int i = 0; i < threadsAmount; i++)
+            {
+                ATM atm = new ATM(25, 60);
+
+                threads[i] = new Thread(atm.Withdraw);
+                threads[i].Name = $"Thread {i}";
+
+                int withdrawsAmount = rnd.Next(8, 13);
+                threads[i].Start(withdrawsAmount);
+                
+            }
+
+            while (threads[threads.Length - 1].IsAlive)
+            {
+                Thread.Sleep(200);
+            }
+            Console.WriteLine($"У вас на счету осталось: {ATM.CurrBalance} рублей");
+        }
+    }
+}
